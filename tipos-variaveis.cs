555621282c47c9e913269c12b4316db0e818f4cb
Camila-blip <59473635+Camila-blip@users.noreply.github.com>
2022-01-06 10:32:48 -0300

    Create tipos-variaveis.cs

---
+++ tipos-variaveis.cs
@@ -0,0 +1,18 @@
+Using System;
+
+namespace Curso{
+  class Programa{
+    static void Main(string[] args){
+      //sbyte minusculo é um tipo pré definido
+      SByte x = 100;
+      byte n1 = 126;
+      int n2 = 1000;
+      long n3 = 2147483648L;
+      bool n4 = false;
+      char n5 = "F";
+      float n6 = 4.5f;
+      double n7 = 4.5;
+      //cw tab tab tab preenche automatico
+    }
+  }
+}
