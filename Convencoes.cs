1ef71caafee2a8b262ecbd928f2d43778aa75223
Camila-blip <59473635+Camila-blip@users.noreply.github.com>
2022-01-07 14:01:29 -0300

    Create Convencoes.cs
    Achei útil anotar para ter uma lembrança.

---
+++ Convencoes.cs
@@ -0,0 +1,4 @@
+//Quando utilizar
+//Camel Case: lastName(parâmetros de métodos, variáveis dentro de métodos)
+//Pascal Case: LastName(namespaces, classe, properties e métodos)
+//Padrão _lastName(atributos "internos" da classe)
