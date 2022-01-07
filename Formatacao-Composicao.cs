af611e08a5980ecf3b67917138193fd9ad65c2a6
Camila-blip <59473635+Camila-blip@users.noreply.github.com>
2022-01-07 14:20:24 -0300

    Update Formatacao-Composicao.cs

--- Formatacao-Composicao.cs
+++ Formatacao-Composicao.cs
@@ -1,1 +1,12 @@
 
+Using System;
+
+namespace Curso{
+  class Programa{
+    static void Main(string[] args){
+      string Nome = "Camila Moura";
+      int Idade = 20;
+      string.Format("Meu nome é {0} e tenho {} anos", nome, idade)
+    }
+  }
+}
