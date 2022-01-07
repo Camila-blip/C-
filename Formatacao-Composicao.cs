50126e2cea011b7a29aab902788ce5fa0fe60c62
Camila-blip <59473635+Camila-blip@users.noreply.github.com>
2022-01-07 14:26:29 -0300

    Update Formatacao-Composicao.cs

--- Formatacao-Composicao.cs
+++ Formatacao-Composicao.cs
@@ -6,7 +6,10 @@ namespace Curso{
     static void Main(string[] args){
       string Nome = "Camila Moura";
       int Idade = 20;
-      string.Format("Meu nome é {0} e tenho {} anos", nome, idade)
+      string.Format("Meu nome é {0} e tenho {} anos", Nome, Idade)
+        //Interpolação
+       Console.WeiteLine($"{Nome} tem {Idade}")
+       Console.WeiteLine("{0} {2:F2} reais", Nome, Idade)
     }
   }
 }
